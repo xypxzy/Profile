347bf5cda3a3238e8a927ebd601884a3a3a1054b
xypxzy <xypxzy@gmail.com>
2022-10-23 15:28:03 +0600

    Finished Content and Footer blocks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-sections");
 
+const slider = document.querySelectorAll(".slide");
+const persons = document.querySelectorAll(".person");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -12,3 +15,19 @@ document.querySelectorAll(".nav").forEach((n) => {
     navMenu.classList.remove("active");
   });
 });
+
+for (let i = 0; i < slider.length; i++) {
+  slider[i].addEventListener("click", () => {
+    slider[i].classList.add("active");
+    if (i > 0) {
+      slider[i - 1].classList.remove("active");
+      persons[0].classList.remove("hidden");
+      persons[2].classList.add("hidden");
+    } else {
+      slider[i + 1].classList.remove("active");
+
+      persons[2].classList.remove("hidden");
+      persons[0].classList.add("hidden");
+    }
+  });
+}
